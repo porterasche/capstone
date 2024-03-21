5f4b0dc3eb9089b2595558d81b9847578c4cc11a
porterasche <porterasche@gmail.com>
2024-03-21 18:40:42 -0500

    fixing script

--- scripts/deploy_ui.sh
+++ scripts/deploy_ui.sh
@@ -1,8 +1,7 @@
-# 
 cd ~/dev/project/capstone/ui
 git pull
 npm ci
 npm run build
 sudo rm -rf /var/www/html
-mkdir /var/www/html
-cp build/. /var/www/html
+sudo mkdir /var/www/html
+sudo cp build/* /var/www/html -r
