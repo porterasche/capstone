2ad9198390de2c8b5ff3e6d959d642a03dd87567
dandydragon <themaskedbaka@gmail.com>
2024-04-03 19:53:11 -0500

    Completed course_convert_dates.json. Semester data is now easy to read.

--- webscraper/course_convert_dates.py
+++ webscraper/course_convert_dates.py
@@ -21,13 +21,43 @@ with open('course_history.json') as f:
         sections = data[class_name]['sections']
         for i in range(len(sections)):
 
-            # These if statements are currently *not* finished. It only assigns Spring courses, and only the season.
-            if (("Jan" in data[class_name]['sections'][i]["Date"] or "Feb" in data[class_name]['sections'][i]["Date"]) or 
-                ("Mar" in data[class_name]['sections'][i]["Date"] or "Apr" in data[class_name]['sections'][i]["Date"]) or
+            # Look through data[class_name]['sections'][i]["Semester"] and calculate whether it is
+            # Spring (Winter sessions count as Spring according to terms), Summer, or Fall.
+            if ("Jan" in data[class_name]['sections'][i]["Date"] or "Feb" in data[class_name]['sections'][i]["Date"] or 
+                "Mar" in data[class_name]['sections'][i]["Date"] or "Apr" in data[class_name]['sections'][i]["Date"] or
                 ("Jan" in data[class_name]['sections'][i]["Date"] and "May" in data[class_name]['sections'][i]["Date"])):       
-                data[class_name]['sections'][i]["Semester"] = "Spring" 
+                data[class_name]['sections'][i]["Semester"] = ""
+                # Can't combine a str and a list together, so we make two temp strings to mash together.
+                # Also, using a regular expression to find the year.
+                temp = "Spring"
+                temp2 = ''.join(re.findall(r"([0-9]{4}) ", data[class_name]['sections'][i]["Date"]))
+                data[class_name]['sections'][i]["Semester"] = ' '.join((temp, temp2))
+
+            elif ("May" in data[class_name]['sections'][i]["Date"] or "Jun" in data[class_name]['sections'][i]["Date"] or 
+                "Jul" in data[class_name]['sections'][i]["Date"] or
+                ("May" in data[class_name]['sections'][i]["Date"] and "Aug" in data[class_name]['sections'][i]["Date"])):       
+                data[class_name]['sections'][i]["Semester"] = ""
+                # Can't combine a str and a list together, so we make two temp strings to mash together.
+                # Also, using a regular expression to find the year.
+                temp = "Summer"
+                temp2 = ''.join(re.findall(r"([0-9]{4}) ", data[class_name]['sections'][i]["Date"]))
+                data[class_name]['sections'][i]["Semester"] = ' '.join((temp, temp2))
+
+            elif ("Aug" in data[class_name]['sections'][i]["Date"] or "Sep" in data[class_name]['sections'][i]["Date"] or 
+                "Oct" in data[class_name]['sections'][i]["Date"] or "Nov" in data[class_name]['sections'][i]["Date"] or
+                ("Aug" in data[class_name]['sections'][i]["Date"] and "Dec" in data[class_name]['sections'][i]["Date"])):       
+                data[class_name]['sections'][i]["Semester"] = ""
+                # Can't combine a str and a list together, so we make two temp strings to mash together.
+                # Also, using a regular expression to find the year.
+                temp = "Fall"
+                temp2 = ''.join(re.findall(r"([0-9]{4}) ", data[class_name]['sections'][i]["Date"]))
+                data[class_name]['sections'][i]["Semester"] = ' '.join((temp, temp2))
+
+            else:
+                data[class_name]['sections'][i]["Semester"] = "ERROR! Likely culprit is course_convert_dates.py!"
+
             print(class_name, sections[i])
 
-updated_json = json.dumps(data)
+# Then, write to course_convert_dates.json
 with open('course_convert_dates.json', 'w') as file:
-    file.write(updated_json)
+    file.write(json.dumps(data, indent=2))
